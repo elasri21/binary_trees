ff03220a3beeef9e109e08e9901a72d45df870e7
elasri21 <islame.edu@gmail.com>
2024-01-03 03:14:40 -0800

    ts 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,8 +9,11 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-if (parent == NULL || node == NULL)
+binary_tree_t *node;
+if (parent == NULL)
+return (NULL);
+node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+if (node == NULL)
 return (NULL);
 node->n = value;
 node->right = NULL;
