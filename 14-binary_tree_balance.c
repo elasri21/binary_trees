6de92141f9b2ee6c7724212904b73e3e2d1366f1
elasri21 <islame.edu@gmail.com>
2024-01-03 11:23:59 -0800

    ts 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,7 +10,7 @@ int tree_height(const binary_tree_t *tree)
 {
 int l_h = 0, r_h = 0;
 if (tree == NULL)
-    return (0);
+return (0);
 l_h = tree->left ? 1 + tree_height(tree->left) : 1;
 r_h = tree->right ? 1 + tree_height(tree->right) : 1;
 return ((l_h > r_h) ? l_h : r_h);
@@ -18,7 +18,7 @@ return ((l_h > r_h) ? l_h : r_h);
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: the binary tree
- * @Return: the balance factor of the tree
+ * Return: the balance factor of the tree
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
