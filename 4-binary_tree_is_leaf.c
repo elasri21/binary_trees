2d2918e4450a47f55a54dedbfe9eb967d0e1160a
elasri21 <islame.edu@gmail.com>
2024-01-03 04:35:49 -0800

    ts 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if node is aleaf
+ * @node: node to be checked
+ * Return: 1 on success.0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node == NULL)
+return (0);
+if (node->left != NULL || node->right != NULL)
+return (0);
+return (1);
+}
